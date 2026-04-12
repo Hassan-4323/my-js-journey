a1a6e8f7d005d6a28a55c0f27595ef4e43acca47
Hassan Jan <hassanzahir2004@gmail.com>
2026-04-12 00:15:58 -0700

    JS async: callbacks, callback hell, and promises practice

---
+++ Chapter 03 OOP API Async/7OOP/73Callbacks_Promises_Async.js
@@ -0,0 +1,70 @@
+// call back: eik function ko agar ap aik aur funtionc baj rahy ho parameter ma to wo parameter wala funtion known as call back
+
+function kuchdeirbaad(fnc) {
+    setTimeout(fnc, Math.floor(Math.random() * 20) * 1000)
+}
+
+kuchdeirbaad(function () {
+    console.log("Hey");
+})
+
+// call hell: aik call back ka andar dusra then so on
+// function profilelakaraw(username, cb) {
+//     console.log("Fetching profile....");
+//     setTimeout(() => {
+//         cb({ _id: 128, username, age: 23, email: "hassanzahir2004@gmail.com" });
+//     }, 2000)
+// }
+// function sarePostLakarAwo(id, cb) {
+//     console.log("Fetching posts....");
+//     setTimeout(() => {
+//         cb({ _id: id, posts: ["hey", "Hello", "Good Morning"] });
+//     }, 3000)
+// }
+
+// function savePostNikalo(id, cb) {
+//     console.log("Fetching saved posts...")
+//     setTimeout(() => {
+//         cb({ _id: id, saved: [1, 2, 3, 4, 5, 6] });
+//     }, 3000)
+// }
+
+// profilelakaraw("Hassan Jan", function (data) {
+//     console.log(data);
+//     sarePostLakarAwo(data._id, function (posts) {
+//         console.log(posts);
+//         savePostNikalo(data._id, function (saved) {
+//             console.log(saved);
+//         });
+//     });
+// });
+
+// practicing call hell
+function profilelaAwo(username,cb){
+    console.log("Fetching profile data...")
+    setTimeout(() => {
+        cb({_id:128,username, age:23, email:"hay@hay.com"})
+    }, 3000);
+}
+function saryPostLaw(id, cb){
+    console.log("Fetching posts...");
+    setTimeout(()=>{
+        cb({_id:id, posts:["Hello", "How", "Are", "You"]});
+    },3000)
+}
+function savePostNikalLo(id, cb){
+    console.log("Fetching saved posts...");
+    setTimeout(()=>{
+        cb({_id:id, saved:[1,2,3,4,5,6]});
+    },3000)
+}
+
+profilelaAwo("Hassan", function(data){
+    console.log(data);
+    saryPostLaw(data._id, function(posts){
+        console.log(posts);
+        savePostNikalLo(data._id, function(saved){
+            console.log(saved);
+        })
+    })
+})
