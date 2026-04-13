edcf3057ef126083b27e44e67cd5493adebb7b95
Hassan Jan <hassanzahir2004@gmail.com>
2026-04-12 19:22:01 -0700

    JS async: implemented async/await examples with try/catch

--- Chapter 03 OOP API Async/7OOP/73Callbacks_Promises_Async.js
+++ Chapter 03 OOP API Async/7OOP/73Callbacks_Promises_Async.js
@@ -1,12 +1,12 @@
 // call back: eik function ko agar ap aik aur funtionc baj rahy ho parameter ma to wo parameter wala funtion known as call back
 
-function kuchdeirbaad(fnc) {
-    setTimeout(fnc, Math.floor(Math.random() * 20) * 1000)
-}
+// function kuchdeirbaad(fnc) {
+//     setTimeout(fnc, Math.floor(Math.random() * 20) * 1000)
+// }
 
-kuchdeirbaad(function () {
-    console.log("Hey");
-})
+// kuchdeirbaad(function () {
+//     console.log("Hey");
+// })
 
 // call hell: aik call back ka andar dusra then so on
 // function profilelakaraw(username, cb) {
@@ -40,31 +40,49 @@ kuchdeirbaad(function () {
 // });
 
 // practicing call hell
-function profilelaAwo(username,cb){
-    console.log("Fetching profile data...")
-    setTimeout(() => {
-        cb({_id:128,username, age:23, email:"hay@hay.com"})
-    }, 3000);
-}
-function saryPostLaw(id, cb){
-    console.log("Fetching posts...");
-    setTimeout(()=>{
-        cb({_id:id, posts:["Hello", "How", "Are", "You"]});
-    },3000)
-}
-function savePostNikalLo(id, cb){
-    console.log("Fetching saved posts...");
-    setTimeout(()=>{
-        cb({_id:id, saved:[1,2,3,4,5,6]});
-    },3000)
-}
+// function profilelaAwo(username, cb) {
+//     console.log("Fetching profile data...")
+//     setTimeout(() => {
+//         cb({ _id: 128, username, age: 23, email: "hay@hay.com" })
+//     }, 3000);
+// }
+// function saryPostLaw(id, cb) {
+//     console.log("Fetching posts...");
+//     setTimeout(() => {
+//         cb({ _id: id, posts: ["Hello", "How", "Are", "You"] });
+//     }, 3000)
+// }
+// function savePostNikalLo(id, cb) {
+//     console.log("Fetching saved posts...");
+//     setTimeout(() => {
+//         cb({ _id: id, saved: [1, 2, 3, 4, 5, 6] });
+//     }, 3000)
+// }
 
-profilelaAwo("Hassan", function(data){
-    console.log(data);
-    saryPostLaw(data._id, function(posts){
-        console.log(posts);
-        savePostNikalLo(data._id, function(saved){
-            console.log(saved);
-        })
-    })
+// profilelaAwo("Hassan", function (data) {
+//     console.log(data);
+//     saryPostLaw(data._id, function (posts) {
+//         console.log(posts);
+//         savePostNikalLo(data._id, function (saved) {
+//             console.log(saved);
+//         })
+//     })
+// })
+
+// =========== promises ==============
+let pr = new Promise(function (res, rej) {
+    setTimeout(() => {
+        let rn = Math.floor(Math.random() * 10);
+        if (rn > 5) res("resoloved " + rn);
+        else rej("rejected " + rn);
+    },3000);
+});
+
+// pr resolve howa then chalyga and reject how to catch chalyga
+
+pr.then(function (val) {
+    console.log(val);
 })
+    .catch(function (val) {
+        console.log(val);
+    });
---
+++ Chapter 03 OOP API Async/7OOP/74Async_Await.js
@@ -0,0 +1,19 @@
+let pr = new Promise(function (res, rej) {
+    setTimeout(() => {
+        let rn = Math.floor(Math.random() * 10);
+        if (rn > 5) res("resoloved " + rn);
+        else rej("rejected " + rn);
+    }, 3000);
+});
+
+async function abcd() {
+    try {
+        let val = await pr;
+        console.log(val);
+    }
+    catch(err){
+        console.log(err);
+    }
+    
+}
+abcd();
