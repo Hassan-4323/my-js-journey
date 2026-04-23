bfb38b6a2a9036d5c2fd20b63cab8c6cbf4a9ad1
Hassan Jan <hassanzahir2004@gmail.com>
2026-04-23 00:49:33 -0700

    JS API: fetch API practice

---
+++ Chapter 03 OOP API Async/7OOP/78API_Practice.js
@@ -0,0 +1,24 @@
+document.getElementById("loadbtn").addEventListener("click", function () {
+    const output = document.getElementById("output");
+    output.innerHTML = "Loading....";
+
+    fetch("https://randomuser.me/api/")
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (data) {
+            const user = data.results[0];
+
+            output.innerHTML = `
+            <img src="${user.picture.large}" alt="User Picture">
+            <p><strong>Name</strong> ${user.name.first} ${user.name.last}</p>
+            <p><strong>Email</strong> ${user.email}</p>
+            <p><strong>Country</strong> ${user.location.country}</p>
+            `;
+        })
+        .catch(function (error) {
+            output.innerHTML = "Error loading user data";
+            console.log(error);
+        });
+});
