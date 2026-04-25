50a4d8cc635459eda5066c44472095cdce806403
Hassan Jan <hassanzahir2004@gmail.com>
2026-04-24 22:46:29 -0700

    Async/Await Project

--- Chapter 03 OOP API Async/7OOP/78API_Practice.js
+++ Chapter 03 OOP API Async/7OOP/78API_Practice.js
@@ -1,24 +1,49 @@
-document.getElementById("loadbtn").addEventListener("click", function () {
+// document.getElementById("loadbtn").addEventListener("click", function () {
+//     const output = document.getElementById("output");
+//     output.innerHTML = "Loading....";
+
+//     fetch("https://randomuser.me/api/")
+//         .then(function (response) {
+//             return response.json();
+//         })
+
+//         .then(function (data) {
+//             const user = data.results[0];
+
+//             output.innerHTML = `
+//             <img src="${user.picture.large}" alt="User Picture">
+//             <p><strong>Name</strong> ${user.name.first} ${user.name.last}</p>
+//             <p><strong>Email</strong> ${user.email}</p>
+//             <p><strong>Country</strong> ${user.location.country}</p>
+//             `;
+//         })
+//         .catch(function (error) {
+//             output.innerHTML = "Error loading user data";
+//             console.log(error);
+//         });
+// });
+
+// Async/Await
+
+document.getElementById("loadbtn").addEventListener("click", async function () {
     const output = document.getElementById("output");
-    output.innerHTML = "Loading....";
+    output.innerHTML = "Loading...";
 
-    fetch("https://randomuser.me/api/")
-        .then(function (response) {
-            return response.json();
-        })
+    try {
+        const response = await fetch("https://randomuser.me/api/");
+        const data = await response.json();
 
-        .then(function (data) {
-            const user = data.results[0];
+        const user = data.results[0];
 
-            output.innerHTML = `
-            <img src="${user.picture.large}" alt="User Picture">
+        output.innerHTML = `
+        <img src="${user.picture.large}" alt="User Picture">
             <p><strong>Name</strong> ${user.name.first} ${user.name.last}</p>
             <p><strong>Email</strong> ${user.email}</p>
             <p><strong>Country</strong> ${user.location.country}</p>
-            `;
-        })
-        .catch(function (error) {
-            output.innerHTML = "Error loading user data";
-            console.log(error);
-        });
+        `
+    }
+    catch (error) {
+        output.innerHTML = "Error loading user data";
+        console.log(error);
+    }
 });
